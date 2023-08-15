c67d9cc8990801d3d416e1d4a54baa8a97ffd3a6
Parker Sprouse <parker@parkersprouse.me>
2023-08-15 00:45:22 -0500

    Begin integrating purgecss - more to be done

--- bin/build.mjs
+++ bin/build.mjs
@@ -4,6 +4,7 @@ import { readFileSync } from 'fs';
 import { dirname, join, resolve } from 'path';
 import postcss from 'postcss';
 import postcssPresetEnv from 'postcss-preset-env';
+import { PurgeCSS } from 'purgecss';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -13,12 +14,12 @@ const __output_dir = join(__root_dir, 'server');
 /**
  * PostCSS
  */
+console.log('STEP 1 - POSTCSS');
 const css_contents = readFileSync(join(__root_dir, 'styles.css'), 'utf8');
-const { css } = await postcss([autoprefixer, postcssPresetEnv()]).process(css_contents, { from: join(__root_dir, 'styles.css') });
+const css_processor = postcss([autoprefixer, postcssPresetEnv()]);
+const { css } = await css_processor.process(css_contents, { from: join(__root_dir, 'styles.css') });
 
-/**
- * Esbuild - Shared Config Options
- */
+/* Esbuild - Shared Config Options */
 const shared = {
   bundle: true,
   loader: {
@@ -35,6 +36,7 @@ const shared = {
 /**
  * Esbuild - CSS
  */
+console.log('STEP 2 - ESBUILD CSS');
 await esbuild.build({
   ...shared,
   outfile: join(__output_dir, 'styles.css'),
@@ -44,11 +46,24 @@ await esbuild.build({
     resolveDir: __output_dir,
     sourcefile: join(__root_dir, 'styles.css'),
   },
+  write: true,
 });
+
+/**
+ * PurgeCSS
+ */
+console.log('STEP 3 - PURGECSS');
+const purged_css_results = await new PurgeCSS().purge({
+  content: [join(__root_dir, '**', '*.html')],
+  css: [join(__output_dir, 'styles.css')],
+  sourceMap: { to: join(__output_dir, 'styles.css.map') },
+});
+console.log(purged_css_results);
 
 /**
  * Esbuild - JS
  */
+console.log('STEP 4 - ESBUILD JS');
 await esbuild.build({
   ...shared,
   entryNames: '[dir]/[name]',
